875482d58fe37df03d451dbfc052143e533ea0a6
Laurent <laurent.pechikoff@gmail.com>
2022-01-24 11:38:01 +0100

    first commit

---
+++ synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/SynchroSaisoSbApplication.java
@@ -0,0 +1,13 @@
+package fr.synchoSaiso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchroSaisoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchroSaisoSbApplication.class, args);
+	}
+
+}
---
+++ synchroSaiso-sb/src/test/java/fr/synchoSaiso/demo/SynchroSaisoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.synchoSaiso.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchroSaisoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
