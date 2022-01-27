57189bd016781e99151d6acd62a4c84e0ee223e3
Laurent <laurent.pechikoff@gmail.com>
2022-01-27 09:59:17 +0100

    effet rotation home component

--- synchroSaiso-ng/src/app/app-routing.module.ts
+++ synchroSaiso-ng/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ActifsComponent } from './components/actifs/actifs.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'about', component:AboutComponent},
-  {path:'actifs', component:ActifsComponent}
+  {path:'actifs', component:ActifsComponent},
+  {path:'admin',component:AdminComponent}
 ];
 
 @NgModule({
