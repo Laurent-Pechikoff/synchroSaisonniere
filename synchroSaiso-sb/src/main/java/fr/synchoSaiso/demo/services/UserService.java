6f684891f01d2a863c6750f529156c420e70e394
Laurent <laurent.pechikoff@gmail.com>
2022-02-01 16:06:25 +0100

    fonctions ajoutees sur UserService

--- synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/services/UserService.java
+++ synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/services/UserService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import fr.synchoSaiso.demo.dao.UserRepository;
 import fr.synchoSaiso.demo.entities.User;
@@ -27,5 +31,15 @@ public class UserService {
 	private User user(@PathVariable ("id") Integer id) {
 		return urp.findById(id).orElse(null);
 	}
+	@DeleteMapping("/deleteUser/{id}")
+	private void deleteUser(@PathVariable ("id") Integer id) {
+		urp.deleteById(id);
+	}
 	
+	@PutMapping("/putUser/{id}")
+	public void putUser(@PathVariable ("id") Integer id,
+			@RequestBody User userUpdate){		
+		userUpdate.setId(id);
+		urp.save(userUpdate);
+	}
 }
