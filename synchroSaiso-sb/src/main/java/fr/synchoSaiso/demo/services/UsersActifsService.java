f8d38000f10f9d96dc9d3790a12be3e9435418a5
Laurent <laurent.pechikoff@gmail.com>
2022-02-02 14:22:24 +0100

    petit test

--- synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/services/UsersActifsService.java
+++ synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/services/UsersActifsService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.synchoSaiso.demo.dao.ActifRepository;
 import fr.synchoSaiso.demo.dao.UsersActifsRepository;
 import fr.synchoSaiso.demo.entities.Actif;
 import fr.synchoSaiso.demo.entities.UsersActifs;
@@ -18,9 +19,14 @@ public class UsersActifsService {
 
 	@Autowired
 	private UsersActifsRepository uarp;
+	@Autowired
+	private ActifRepository arp;
+	
 	
 	@GetMapping("/getActifsByUser/{id}")
 	private List<UsersActifs> getActifsByUserId(@PathVariable ("id") Integer id){
+		List<UsersActifs> result = uarp.findByUserId(id);
+		
 		return uarp.findByUserId(id);
 		
 	}
