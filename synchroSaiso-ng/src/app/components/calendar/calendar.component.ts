4b788dbd7eefa98630a0ac6f3702e7fd224b14d2
d2la <patrick.louise@hotmail.fr>
2022-02-04 23:45:16 +0100

    event calendar

--- synchroSaiso-ng/src/app/components/calendar/calendar.component.ts
+++ synchroSaiso-ng/src/app/components/calendar/calendar.component.ts
@@ -13,7 +13,23 @@ export class CalendarComponent implements OnInit {
  dataAirbnb:any
  dataIcal:any
 
-  Events: any = [];
+ icalEvent={
+  title:'',
+  start:'',
+  end:'',
+  startHour:14,
+  endHour:11,
+  urlOrigin:'',
+  idOrigin:'',
+  actif:{}
+}
+
+
+
+
+  Events: any = [
+    {titre:'test1', start:'2022-02-15'},{titre:'test2', start:'2022-02-16', end:'2022-02-18'}
+  ];
   calendarOptions: CalendarOptions | undefined;
    
   constructor(private cs:CalendarService) { }
@@ -38,7 +54,13 @@ export class CalendarComponent implements OnInit {
       this.calendarOptions = {
         initialView: 'dayGridMonth',
         dateClick: this.onDateClick.bind(this),
-        events: this.Events
+        events: this.Events,
+        eventClick: function(info){
+          info.jsEvent.preventDefault();//empecher le changement d'url sur le meme onglet
+          if (info.event.url) {
+            window.open(info.event.url);//ouverture nouvel onglet sur url
+          }
+        }
       };
     }, 2500);
          
@@ -53,46 +75,54 @@ export class CalendarComponent implements OnInit {
     }
   }
   
-  icalEvent={
-    title:'',
-    start:'',
-    end:'',
-    startHour:14,
-    endHour:11,
-    urlOrigin:'',
-    idOrigin:'',
-    actif:{}
-  }
 
-  convertFrenchDate(date:any){
-    let year= date.substr(0,4)
-    let month= date.substr(4,2)
-    let day= date.substr(6,2)
-    return year+'-'+month+'-'+day
-  }
 
   getIcal(){
     this.cs.getIcal().subscribe(resp=>{
       this.dataIcal=resp
       this.dataIcal=this.dataIcal.vcalendar[0].vevent
-      console.log(this.dataIcal.length)
       for(let i=0;i<this.dataIcal.length;i++){ 
-          let desc=''
-              for(let j=0;j<this.dataIcal[i].description.length;j++){
-                if(this.dataIcal[i].description[j]=='\\'){break}
-                if(j>16){ desc+=this.dataIcal[i].description[j]}
-              }
-              this.icalEvent.title=desc
-          this.icalEvent.start=this.convertFrenchDate(this.dataIcal[i].dtstart[0]);
-          this.icalEvent.end=this.convertFrenchDate(this.dataIcal[i].dtend[0]);
-          this.icalEvent.idOrigin=this.dataIcal[i].uid;
-              this.Events[i]= this.icalEvent
-              console.log(this.Events)
-
-              
+        if("description" in this.dataIcal[i]){ 
+          this.Events.push( this.icalToEvent(i))
+        }
       }
     })
   }
+
+
+icalToEvent( i:any){
+  let icalEvent2={
+    title:'',
+    start:'',
+    end:'',
+    startHour:14,
+    endHour:11,
+    url:'',
+    idOrigin:'',
+  } 
+  let desc=''
+  icalEvent2.title='AirBnb'
+      for(let j=0;j<this.dataIcal[i].description.length;j++){
+        if(this.dataIcal[i].description[j]=='\\'){break}
+        if(j>16){ desc+=this.dataIcal[i].description[j]}
+      }
+      icalEvent2.url=desc
+  icalEvent2.start=this.convertFrenchDate(this.dataIcal[i].dtstart[0]);
+  icalEvent2.end=this.convertFrenchDate(this.dataIcal[i].dtend[0]);
+  icalEvent2.idOrigin=this.dataIcal[i].uid;
+  return icalEvent2
+    
+  }
+
+
+
+convertFrenchDate(date:any){
+  let year= date.substr(0,4)
+  let month= date.substr(4,2)
+  let day= date.substr(6,2)
+  return year+'-'+month+'-'+day
+}
+
 
 
 }
---
+++ synchroSaiso-sb/src/main/java/fr/synchoSaiso/demo/services/EntityTestService.java
@@ -0,0 +1,24 @@
+package fr.synchoSaiso.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.synchoSaiso.demo.dao.EntityTestRepository;
+import fr.synchoSaiso.demo.entities.EntityTest;
+import fr.synchoSaiso.demo.entities.User;
+
+@RestController
+@CrossOrigin("*")
+public class EntityTestService {
+	@Autowired
+	private EntityTestRepository etrp;
+	
+	@PostMapping("/addTest")
+	public void addTest(@RequestBody EntityTest test) {
+		etrp.save(test);
+	}
+	
+}
